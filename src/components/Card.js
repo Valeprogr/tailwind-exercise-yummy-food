5cbafeef6b7df4342cae4036ed022301fb909b6a
valentina vittoria <valeviit@icloud.com>
2023-03-24 14:39:11 +0100

    create login, signup and FavoriteRecipes compoonen

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,26 @@
 import React from 'react';
-const Card = ({props}) => {
+const Card = ({ props }) => {
+        return (
+            <>
+                <div className='card hover:shadow-lg cursor-pointer'>
+                    <img src={props.img} alt='recipe' className='w-full  h-32 sm:h-48 object-cover'></img>
+                    <div className='m-4'>
+                        <span className='font-bold'>{props.name}</span>
+                        <span className='block text-gray-500 text-sm'>{props.author}</span>
+                    </div>
+                    <div className='badge flex justify-between'>
+                        <svg className='w-5' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+
+                        <span className='ml-1'>{props.time}</span>
+                    </div>
+                    <div className='flex justify-center mb-5'>
+                        <button type='button' className='bg-secondary-100 text-secondary-200 btn hover:shadow-inner transform hover:scale-125 hover:bg-opacity-50 transition ease-out duration-300 w-36 text-center'>Save</button>
+                    </div>
     
-    return (
-        <>
-            <div className='card hover:shadow-lg cursor-pointer'>
-            <img src={props.img} alt='recipe' className='w-full  h-32 sm:h-48 object-cover'></img>
-            <div className='m-4'>
-                    <span className='font-bold'>{ props.name}</span>
-                    <span className='block text-gray-500 text-sm'>{ props.author}</span>
-            </div>
-                <div className='badge flex justify-between'>
-                <svg className='w-5' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-</svg>
-
-                    <span className='ml-1'>{ props.time}</span>
                 </div>
-                <div className='flex justify-center mb-5'>
-                <div className='bg-secondary-100 text-secondary-200 btn hover:shadow-inner transform hover:scale-125 hover:bg-opacity-50 transition ease-out duration-300 w-36 text-center'>Save</div>
-                </div>
-                
-        </div>
-        </>
-    );
-}
-
+            </>
+        );
+    }
 export default Card;
--- src/pages/FavoriteRecipe.js
+++ src/pages/FavoriteRecipe.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const FavoriteRecipe = () => {
+    // useEffect(() => {
+    //     const data = localStorage.getItem('recipe');
+    //     const recipes = JSON.parse(data);
+    //     console.log(recipes)
+    // }, []);
+
     return (
         <div className='text-gray-600 font-body bg-gray-100 flex max-h-full justify-center'>
             <div className='mt-8 mb-3 text-5xl font-semibold '>
